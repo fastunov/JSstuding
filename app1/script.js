40f0a2392c2d7103cd8ec2c8d645cff50939309a
Sergey Fatunov <fastunov@gmail.com>
2020-04-21 15:42:35 +0400

    Задание 1

--- app1/script.js
+++ app1/script.js
@@ -1,8 +1,23 @@
-'use strict';
+"use strict";
 
-var leftBorderWidth = 1;
-let counter = 0;
-const pi = 3.1415;
+let money = prompt ("Ваш бюджет на месяц?");
+let time = prompt ("Введите дату в формате YYYY-MM-DD");
 
-console.log('finished');
+let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses : {},
+    income:[],
+    savings : false
+};
 
+let answer11 = prompt("Введите обязательную статью расходов в этом месяце"),
+    answer12 = prompt("Во сколько обойдется?"),
+    answer21 = prompt("Введите обязательную статью расходов в этом месяце"),
+    answer22 = prompt("Во сколько обойдется?");
+
+appData.expenses[answer11] = answer12;
+appData.expenses[answer21] = answer22;
+
+alert(appData.budget / 30);
