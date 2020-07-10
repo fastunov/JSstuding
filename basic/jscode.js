a2f028844b4fb478df756015683b05693eaf5a83
Sergey Fatunov <fastunov@gmail.com>
2020-07-10 15:46:57 +0400

    String lessons

--- basic/jscode.js
+++ basic/jscode.js
@@ -1,8 +1,18 @@
-'use strict';
+'use strict'
 
-var peremvar = 'Hobla'
-const peremconst = 'Tapuli'
-let peremlet = 'Chupiko'
+function convertString (s, name, age) {
+    if (age <= 0 ) {
+        age = 'Еще не родился!'
+    }
+    return `${s[0]}${name}${s[1]}${age}${s[2]}`
+}
 
-console.log (peremvar)
+const name1 = 'Петрушка'
+const age1 = 30
 
+console.log (convertString`Имя: ${name1} Возраст ${age1}`)
+
+const name2 = 'Фолгер'
+const age2 = -10
+
+console.log (convertString`Имя: ${name2} Возраст ${age2}`)
