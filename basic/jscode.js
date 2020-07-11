eac90b2a9245c98b6ca6ff3cfce44c914563d0c2
Sergey Fatunov <fastunov@gmail.com>
2020-07-11 19:06:43 +0400

    Last snhages for Basic Course

--- basic/jscode.js
+++ basic/jscode.js
@@ -1,18 +1,3 @@
-'use strict'
+const heading = document.getElementById('heading')
 
-function convertString (s, name, age) {
-    if (age <= 0 ) {
-        age = 'Еще не родился!'
-    }
-    return `${s[0]}${name}${s[1]}${age}${s[2]}`
-}
-
-const name1 = 'Петрушка'
-const age1 = 30
-
-console.log (convertString`Имя: ${name1} Возраст ${age1}`)
-
-const name2 = 'Фолгер'
-const age2 = -10
-
-console.log (convertString`Имя: ${name2} Возраст ${age2}`)
+console.dir(heading)
