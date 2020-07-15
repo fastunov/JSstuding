c12646289dc278b68d8e6a013b051ef4c9db8ae9
Sergey Fatunov <fastunov@gmail.com>
2020-07-15 14:19:51 +0400

    Cide test for several JavaScript lessons

--- basic/jscode.js
+++ basic/jscode.js
@@ -1,3 +1,43 @@
-const heading = document.getElementById('heading')
+// // const heading = document.getElementById('heading')
 
-console.dir(heading)
+// // console.dir(heading)
+
+// // function hello() {
+// // 	console.log('Hello!', this)
+// // }
+
+
+// // const person = {
+// // 	name: 'Serg',
+// // 	age: 48,
+// // 	sayhello: hello
+// // }
+
+// // function createCalcFunction(n) {
+// // 	return function() {
+// // 		console.log(1000 * n)
+// // 	}
+// // }
+
+// // const calc = createCalcFunction(34)
+// // calc()
+
+// // function simpleInc (n) {
+// // 	return function (num) {;
+// // 		return num + n;
+// // 	};
+// // };
+
+// // const addfive = simpleInc ();
+
+// function bind(){
+// 	return 
+// }
+
+// console.log('Test')
+
+function bind(context, fn) {
+	return function (..args) {
+		fn.apply(context, args)
+	}
+}
